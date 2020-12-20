e62ba7897f39443e0581982ed73999d71130de56
bmalkmus <bmalkmus23@gmail.com>
2020-12-19 20:33:45 -0800

    all tests completed

--- test/joke-test.js
+++ test/joke-test.js
@@ -1,14 +1,48 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
-const { isCallTrace } = require("hardhat/internal/hardhat-network/stack-traces/message-trace");
 
 describe("JokeList", function(){
-    it("Should have a start-up list of 4 jokes", async function(){
+        let jokeslist;
+    beforeEach(async ()=>{
         const JokeList = await hre.ethers.getContractFactory("JokesList");
-        const jokes = await JokeList.deploy()
+        jokeslist = await JokeList.deploy()
 
-        await jokes.deployed();
-        expect(await jokes.getJokeCount()).to.equal(4);
+        await jokeslist.deployed();
+    })
+    it("Should have a start-up list of 4 jokes", async function(){
+        expect(await jokeslist.getJokeCount()).to.equal(4);
+    })
+
+    it("Should have a joke count of 5 when joke is added", async function(){
+        await jokeslist.addJoke("How do celebrities stay cool?", "They have many fans");
+        const joke = await jokeslist.jokes(5)
+        const count = await jokeslist.getJokeCount()
+
+        expect(count).to.equal(5)
+        expect (joke.punchLine).to.equal("They have many fans")
+    })
+
+    it("Should increase votes amount when funny", async function(){
+        let joke = await jokeslist.jokes(1)
+        await jokeslist.jokeFunny(joke.id)
+
+        joke = await jokeslist.jokes(1);
+
+        expect(joke.votes).to.equal(1);
+    })
+
+    it("Should decrease votes amount when not funny" , async function(){
+        let joke = await jokeslist.jokes(1)
+        await jokeslist.jokeFunny(joke.id);
+        await jokeslist.jokeFunny(joke.id);
+        await jokeslist.jokeFunny(joke.id);
+        await jokeslist.jokeNotFunny(joke.id);
+
+        joke = await jokeslist.jokes(1);
+
+        expect(joke.votes).to.equal(2)
     })
 });
+
+
 
